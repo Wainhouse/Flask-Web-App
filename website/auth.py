df1db2a1a216cda415b348b2af301366c4eae37f
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-10-11 10:27:44 +0100

    WW: auth.py error fixed

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,16 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/')
-def home():
-    return "<h1>Test</h1>"
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>sign-up</p>"
+
+
