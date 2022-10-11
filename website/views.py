7bbe6d4d15e2561b49dd01525f4d75b89a7fd96d
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-10-11 10:39:56 +0100

    WW: created base.html, added comments throughout

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 
+# the blueprint for this page
 views = Blueprint('views', __name__)
 
-
+# Create the url route
 @views.route('/')
 def home():
     return "<h1>Test</h1>"
