d1ebaa9966108dc98856266aa9a74bb1f1201107
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-10-17 10:19:31 +0100

    WW: Configured flash alerts

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,9 @@
-from xmlrpc.client import boolean
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 # each page/url this app will use
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean = True)
 
@@ -12,8 +11,25 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+       email = request.form.get('email')
+       firstName = request.form.get('firstName')
+       pasword1 = request.form.get('password1')
+       password2 = request.form.get('password2')
+
+       if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+       elif len(firstName) < 2:
+            flash('Firstname must be greater than 2 characters.', category='error')
+       elif pasword1 != password2:
+            flash('Passwords don\'t match.', category='error')
+       elif len(pasword1) < 7:
+            flash('Password too short, must be 7 characters.', category='error')
+       else:
+            flash('Account Created.', category='success')
+
     return render_template("sign_up.html")
 
 
