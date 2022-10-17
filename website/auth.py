314340aa29d16b0250b090156a531f2c67cf81ab
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-10-17 11:42:06 +0100

    WW: Configured db and sign up

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,26 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 # each page/url this app will use
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+                flash('Email does not exist.', category='error')
+
     return render_template("login.html", boolean = True)
 
 @auth.route('/logout')
@@ -19,7 +35,10 @@ def sign_up():
        pasword1 = request.form.get('password1')
        password2 = request.form.get('password2')
 
-       if len(email) < 4:
+       user = User.query.filter_by(email=email).first()
+       if user:
+            flash('Email already exists.', category='error')
+       elif len(email) < 6:
             flash('Email must be greater than 4 characters.', category='error')
        elif len(firstName) < 2:
             flash('Firstname must be greater than 2 characters.', category='error')
@@ -28,7 +47,12 @@ def sign_up():
        elif len(pasword1) < 7:
             flash('Password too short, must be 7 characters.', category='error')
        else:
+            new_user = User(email=email, first_name=firstName, password= generate_password_hash(pasword1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()   
             flash('Account Created.', category='success')
+            return redirect(url_for('views.home'))
+            
 
     return render_template("sign_up.html")
 
--- website/models.py
+++ website/models.py
@@ -2,16 +2,18 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy import func
 
+
+# Note table 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.foreignKey('user.id'))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
+# User table
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.column(db.String(150), unique=True)
-    password = db.column(db.String(150))
-    firstName = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
