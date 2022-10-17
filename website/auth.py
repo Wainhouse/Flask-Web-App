444e41d10646506fdc3b8e531aba096181617c19
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-10-17 14:20:27 +0100

    WW: Login/ Logout funtionality added

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -16,16 +17,21 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+               #  remember user is logged in and redirect to home
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
                 flash('Email does not exist.', category='error')
 
-    return render_template("login.html", boolean = True)
+    return render_template("login.html", user= current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+     logout_user()
+     return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -35,6 +41,7 @@ def sign_up():
        pasword1 = request.form.get('password1')
        password2 = request.form.get('password2')
 
+# Functionality for the user's account'
        user = User.query.filter_by(email=email).first()
        if user:
             flash('Email already exists.', category='error')
@@ -54,6 +61,6 @@ def sign_up():
             return redirect(url_for('views.home'))
             
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
 
 
--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
+from flask_login import  login_required, current_user
+
 
 # the blueprint for this page
 views = Blueprint('views', __name__)
 
 # Create the url route
 @views.route('/')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
