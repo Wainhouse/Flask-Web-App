30f7d02d9f46cdaa3983a3c50632f98f41d92095
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-10-17 14:52:40 +0100

    WW: Final changes in DB

--- website/__init__.py
+++ website/__init__.py
@@ -23,6 +23,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
+    from . import models
 
     with app.app_context():
         db.create_all()
--- website/views.py
+++ website/views.py
@@ -1,12 +1,38 @@
-from flask import Blueprint, render_template
-from flask_login import  login_required, current_user
-
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 # the blueprint for this page
 views = Blueprint('views', __name__)
 
 # Create the url route
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note Added!', category='success')
     return render_template("home.html", user=current_user)
+
+# Confirms the not is the current users, and allows for user to delete notes
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
